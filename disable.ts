22a37cfb472084db3474e2a92712d60de232cfce
hota1024 <hotalog@hota1024.com>
2020-08-02 15:40:15 +0900

    🏷️ Update disable.ts

--- disable.ts
+++ disable.ts
@@ -1,11 +1,12 @@
+console.log(
+  '%cDisabled%cReact Developer Tools',
+  'padding: 10px; border-radius: 6px 0 0 6px; background: #f44336; color: #FFCDD2;',
+  'padding: 10px; border-radius: 0 6px 6px 0; background: black; color: #03A9F4;'
+)
+
 declare let __REACT_DEVTOOLS_GLOBAL_HOOK__: {
   inject(): void
 }
 
-__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = () => {
-  console.log(
-    '%cDisabled%cReact Developer Tools',
-    'padding: 10px; border-radius: 6px 0 0 6px; background: #f44336; color: #FFCDD2;',
-    'padding: 10px; border-radius: 0 6px 6px 0; background: black; color: #03A9F4;'
-  )
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = () => {}
